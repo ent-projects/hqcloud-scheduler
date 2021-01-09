e78a98d79058a9b309fa17b2b125a12db44bee85
Heng WU <wuheng@iscas.ac.cn>
2021-01-09 17:56:40 +0800

    add hqcloud-scheduler

--- src/main/java/com/hqcloud/scheduler/Scheduler.java
+++ src/main/java/com/hqcloud/scheduler/Scheduler.java
@@ -6,8 +6,12 @@ package com.hqcloud.scheduler;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Random;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.github.kubesys.KubernetesClient;
 import com.github.kubesys.KubernetesWatcher;
 import com.hqcloud.scheduler.utils.ClientUtil;
@@ -89,8 +93,9 @@ public class Scheduler {
 		}
 
 		@Override
-		public void doAdded(JsonNode node) {
-			Arrays.sort(getAllNodes(), new Comparator<JsonNode>() {
+		public void doAdded(JsonNode trigger) {
+			JsonNode[] nodes = getAllNodes();
+			Arrays.sort(nodes, new Comparator<JsonNode>() {
 
 				@Override
 				public int compare(JsonNode o1, JsonNode o2) {
@@ -100,6 +105,7 @@ public class Scheduler {
 				}
 				
 			});
+			JsonNode job = toJob(trigger, nodes[0].get("metadata").get("name").asText());
 		}
 
 		@Override
@@ -164,4 +170,100 @@ public class Scheduler {
 		}
 		return value.asText();
 	}
+	
+
+	protected static JsonNode toJob(JsonNode trigger, String nodeName) {
+		
+		String name = trigger.get("metadata").get("name").asText()
+				      + "-" + getRandomString();
+		
+		ObjectNode job = new ObjectMapper().createObjectNode();
+		job.put("apiVersion", "batch/v1");
+		job.put("kind", "Job");
+		
+		{
+			ObjectNode meta = new ObjectMapper().createObjectNode();
+			meta.put("name", name);
+			{
+				ObjectNode labels = new ObjectMapper().createObjectNode();
+				labels.put("app", name);
+				labels.put("trigger", trigger.get("metadata").get("name").asText());
+				meta.set("labels", labels);
+			}
+			job.set("metadata", meta);
+		}
+		
+		{
+			ObjectNode spec = new ObjectMapper().createObjectNode();
+			{
+				ObjectNode temp = new ObjectMapper().createObjectNode();
+				
+				{
+					ObjectNode meta = new ObjectMapper().createObjectNode();
+					{
+						ObjectNode labels = new ObjectMapper().createObjectNode();
+						labels.put("app", name);
+						meta.set("labels", labels);
+					}
+					temp.set("metadata", meta);
+				}
+				
+				{
+					ObjectNode spec2 = new ObjectMapper().createObjectNode();
+					
+					{
+						ArrayNode containers = new ObjectMapper().createArrayNode();
+						
+						{
+							ObjectNode container = new ObjectMapper().createObjectNode();
+							container.put("image", getImage(trigger.get("sink").asText()));
+							container.put("name", name);
+							container.put("imagePullPolicy", "IfNotPresent");
+							
+							if (trigger.get("info").has("env")) {
+								container.set("env", trigger.get("info").get("env"));
+							}
+							
+							if (trigger.get("info").has("command")) {
+								container.set("command", trigger.get("info").get("command"));
+							}
+							
+							containers.add(container);
+						}
+						spec2.set("containers", containers);
+					}
+					
+					temp.set("spec", spec2);
+				}
+				spec.set("template", temp);
+			}
+			
+			job.set("spec", spec);
+		}
+		return job;
+	}
+	
+	 protected static String getRandomString(){
+	     String str="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	     Random random=new Random();
+	     StringBuffer sb=new StringBuffer();
+	     for(int i=0;i<6;i++){
+	       int number=random.nextInt(62);
+	       sb.append(str.charAt(number));
+	     }
+	     return sb.toString();
+	 }
+	 
+	 protected static String getImage(String key) {
+		 try {
+			JsonNode json = client.getResource("ConfigMap", "default", "hqcloud-algorithm");
+			return json.get("data").get(key).asText();
+		} catch (Exception e) {
+			return null;
+		}
+	 }
+
+	 public static void main(String[] args) throws Exception {
+		 new Scheduler();
+	 }
 }
